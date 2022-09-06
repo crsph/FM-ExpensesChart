afa0042ac0819e9e566aafb9011be711a9516851
CaffeineProgrammer <crsph@users.noreply.github.com>
2022-09-06 22:12:56 +0200

    WIP

--- components/BarStatistic.js
+++ components/BarStatistic.js
@@ -1,16 +1,51 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import styles from "../styles/BarStatics.module.scss";
 
 export default function BarStatistic(props) {
-  const days = ["mon", "tue", "wed", "thu", "fri", "sat", "sun"];
+  const dailyExpenseObjectArray = [
+    { day: "mon", expense: 0.0, percentage: "" },
+    { day: "tue", expense: 0.0, percentage: "" },
+    { day: "wed", expense: 0.0, percentage: "" },
+    { day: "thu", expense: 0.0, percentage: "" },
+    { day: "fri", expense: 0.0, percentage: "" },
+    { day: "sat", expense: 0.0, percentage: "" },
+    { day: "sun", expense: 0.0, percentage: "" },
+  ];
+
+  const [dailyExpenseArray, setDailyExpenseArray] = useState(
+    dailyExpenseObjectArray
+  );
+
+  useEffect(() => {
+    setDailyExpenseArray(
+      dailyExpenseArray.map((dailyExpenseObj) => {
+        const weeklyExpense = props.spending / 4;
+        const percentage = Math.random() * (1 - 0);
+        // console.log(percentage);
+        const dailySpending = (weeklyExpense / 7) * percentage;
+
+        return {
+          ...dailyExpenseObj,
+          expense: dailySpending.toFixed(2),
+          percentage: (percentage * 100) + "%",
+        };
+      })
+    );
+  }, []);
+
+  // console.log(dailyExpenseArray);
 
   return (
     <>
       <div className={styles["weekly-stat-container"]}>
-        {days.map((day, index) => (
-          <div key={index}>
-            <div className={styles["weekly-stat-container__bar"]}/>
-            {day}
+        {dailyExpenseArray.map((dayObject, index) => (
+          <div key={index} className={styles["daily-stat-container"]}>
+            <div
+              className={styles["daily-stat-container__bar"]}
+              style={{ height: dayObject.percentage }}
+            />
+            {/* {dayObject.expense} */}
+            <p>{dayObject.day}</p>
           </div>
         ))}
       </div>
